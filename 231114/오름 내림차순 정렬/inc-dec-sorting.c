0325be9dd202e9393b8608a10ddc68cb3502d86c
김진우 <97828117+rlawlsdn2797@users.noreply.github.com>
2023-11-14 17:17:14 +0900

    Title: 오름 내림차순 정렬, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 231114/오름 내림차순 정렬/inc-dec-sorting.c
+++ 231114/오름 내림차순 정렬/inc-dec-sorting.c
@@ -1,36 +1,29 @@
 #include <stdio.h>
 
 int main() {
-    int n, i, j, tmp, arr[103], arr2[103];
+    int n, i, j, tmp, arr[103];
 
     scanf("%d", &n);
 
     for(i = 1; i <= n; i++) {
         scanf("%d", &arr[i]);
-        arr2[i] = arr[i];
     }
 
     for(i = 1; i <= n; i++) {
         for(j = i + 1; j <= n; j++) {
-            if(arr[i] < arr[j]) {
+            if(arr[i] > arr[j]) {
                 tmp = arr[i];
                 arr[i] = arr[j];
                 arr[j] = tmp;
-            }
-
-            if(arr2[i] > arr2[j]) {
-                tmp = arr2[i];
-                arr2[i] = arr2[j];
-                arr2[j] = tmp;
             }
         }
     }
 
     for(i = 1; i <= n; i++) {
-        printf("%d ", arr2[i]);
+        printf("%d ", arr[i]);
     }
     printf("\n");
-    for(i = 1; i <= n; i++) {
+    for(i = n; i >= 1; i--) {
         printf("%d ", arr[i]);
     }
 

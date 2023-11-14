97c2f22ac53664aab39c416cc549f405445178cf
김진우 <97828117+rlawlsdn2797@users.noreply.github.com>
2023-11-14 17:31:50 +0900

    Title: hashmap 기본, Time: 112ms, Memory: 0MB, Status: Passed - CodeTree

--- 231114/hashmap 기본/hashmap-basic.cpp
+++ 231114/hashmap 기본/hashmap-basic.cpp
@@ -1,55 +1,39 @@
 #include <iostream>
 #include <string.h>
-#include <vector>
+#include <unordered_map>
 
 using namespace std;
 
 int main() {
     int n, a, b, i;
-    char str[10];
+    string str;
 
-    vector<pair<int, int> > arr;
+    unordered_map<int, int> arr;
 
     cin >> n;
 
     while(n--) {
         cin >> str;
 
-        if(strcmp(str, "add") == 0) {
-            scanf("%d %d", &a, &b);
+        if(str == "add") {
+            cin >> a >> b;
 
-            for(i = 0; i < arr.size(); i++) {
-                if(arr[i].first == a) {
-                    arr[i].second = b;
-                }
-            }
+            arr[a] = b;
 
-            if(i >= arr.size()) {
-                arr.push_back({a, b});
-            }
         }
-        else if(strcmp(str, "find") == 0) {
-            scanf("%d", &a);
-            
-            for(i = 0; i < arr.size(); i++) {
-                if(arr[i].first == a) {
-                    cout << arr[i].second << endl;
-                    break;
-                }
-            }
-            
-            if(i >= arr.size()) {
-                cout << "None" << endl;
-            }
+        else if(str == "remove") {
+            cin >> a;
+
+            arr.erase(a);
         }
         else {
-            scanf("%d", &a);
+            cin >> a;
 
-            for(i = 0; i < arr.size(); i++) {
-                if(arr[i].first == a) {
-                    arr.erase(arr.begin() + i);
-                    break;
-                }
+            if(arr.find(a) == arr.end()) {
+                cout << "None" << endl;
+            }
+            else {
+                cout << arr[a] << endl;
             }
         }
     }

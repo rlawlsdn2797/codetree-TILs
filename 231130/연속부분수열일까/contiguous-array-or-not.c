92b4c124dc38391297745e36631d5057f4839b92
김진우 <97828117+rlawlsdn2797@users.noreply.github.com>
2023-11-30 17:18:31 +0900

    Title: 연속부분수열일까, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 231130/연속부분수열일까/contiguous-array-or-not.c
+++ 231130/연속부분수열일까/contiguous-array-or-not.c
@@ -1,8 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int n1, n2;
+
+int Func(int *arr1, int *arr2) {
+    int i;
+    for(int i = 0; i < n1; i++) {
+        int j;
+
+        for(j = 0; j < n2; j++) {
+            if(arr1[i+j] != arr2[j]) {
+                break;
+            }
+        }
+
+        if(j >= n2) {
+            return 1;
+        }
+    }
+
+    return 0;
+}
+
 int main() {
-    int n1, n2;
     int *arr1, *arr2;
 
     scanf("%d %d", &n1, &n2);
@@ -18,21 +38,12 @@ int main() {
         scanf("%d", &arr2[i]);
     }
 
-    for(int i = 0; i < n1; i++) {
-        int j;
-
-        for(j = 0; j < n2; j++) {
-            if(arr1[i+j] != arr2[j]) {
-                break;
-            }
-        }
-
-        if(j >= n2) {
-            printf("Yes");
-            return 0;
-        }
+    if(Func(arr1, arr2) == 1) {
+        printf("Yes");
+    }
+    else {
+        printf("No");
     }
 
-    printf("No");
     return 0;
 }

be3e3bb4f4e074b11e36bd5325310e102d46ed25
김진우 <97828117+rlawlsdn2797@users.noreply.github.com>
2023-12-07 13:11:08 +0900

    Title: 2가 3번째로 등장하는 위치, Time: 6ms, Memory: 0MB, Status: Runtime Error - CodeTree

--- 231207/2가 3번째로 등장하는 위치/where-2-appears-3rd.c
+++ 231207/2가 3번째로 등장하는 위치/where-2-appears-3rd.c
@@ -1,14 +1,20 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main() {
-    int n, r, res = 0;
+    int n, r, res = 0, i;
+    int *arr;
 
     scanf("%d", &n);
 
-    for(int i = 1; i <= n; i++) {
-        scanf("%d", &r);
+    arr = (int*)malloc(sizeof(int) * n);
 
-        if(r == 2) res++;
+    for(i = 1; i <= n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    for(int i = 1; i <= n; i++) {
+        if(arr[i] == 2) res++;
 
         if(res == 3) {
             printf("%d ", i);
